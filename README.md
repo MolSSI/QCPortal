49fc5cb1c7fa8d6e0490a31b1f89b3324e00864e
Benjamin Pritchard <ben@bennyp.org>
2021-09-19 10:52:22 -0400

    Initial commit of files

---
+++ README.md
@@ -0,0 +1,37 @@
+QCPortal
+==============================
+[//]: # (Badges)
+[![Continuous Integration](https://github.com/MolSSI/QCPortal/workflows/CI%20workflow%20for%20GHA/badge.svg)](https://github.com/MolSSI/QCPortal/actions?query=workflow%3A%22CI+workflow+for+GHA%22)
+
+A client interface to the QC Archive Project.
+
+This standalone client serves to hook into QCFractal instances from remote or local 
+locations and is designed for end-users who which to access the QCFractal stores.
+
+### Issues
+
+All issues should be reported to the [QCFractal issue tracker](https://github.com/MolSSI/QCFractal/issues/new/choose).
+This repository is automatically updated from the main QCFractal source code and is only used for deployment of the QCPortal package.
+
+### QCPortal Version descriptions with QCFractal
+
+QCPortal's release versions mostly match QCFractal's releases with the following standard form
+
+```X.Y.Z```
+
+for Major (X), Minor(Y), and Patch(Z) version respectively. However, because Portal is intrinsicly linked to
+Fractal, the following rules must be adhered to:
+1. Major release version **must** be the same as Fractal
+2. Minor release version **must** be the same as Fractal
+3. Patch version is unrestricted and can differ from Fractal
+
+### Copyright
+
+Copyright (c) 2018, Molecular Software Sciences Institute (MolSSI)  
+
+
+#### Acknowledgements
+ 
+Project based on the 
+[Computational Chemistry Python Cookiecutter](https://github.com/choderalab/cookiecutter-python-comp-chem)
+
---
+++ setup.py
@@ -0,0 +1,59 @@
+"""
+QCPortal
+A client interface to the QC Archive Project
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+short_description = "A client interface to the QC Archive Project."
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except FileNotFoundError:
+    long_description = short_description
+
+setup(
+    # Self-descriptive entries which should always be present
+    name='qcportal',
+    author='MolSSI',
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    python_requires=">=3.7",
+    license='BSD-3-Clause',
+    # Which Python importable modules should be included when your package is installed
+    packages=find_packages(),
+    include_package_data=True,
+
+    install_requires=[
+        'msgpack>=0.6.1',
+        'numpy>=1.7',
+        'pandas',
+        'requests',
+        'pyyaml>=5.1',
+        'pydantic>=1.0.0',
+        'qcelemental>=0.11.0',
+        'tqdm',
+        'h5py',
+        'pyarrow>=0.13.0',
+        'plotly',
+        'nglview'
+    ],
+
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
+    zip_safe=True,
+)
